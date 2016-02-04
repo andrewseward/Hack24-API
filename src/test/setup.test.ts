1a9805d57477159c0e87c2ef8e844e1ee6527357
David Wood <david.p.wood@gmail.com>
2016-02-04 14:20:28 +0000

    Fail test when spawn fails

--- src/test/setup.test.ts
+++ src/test/setup.test.ts
@@ -1,3 +1,4 @@
+import * as assert from 'assert'
 import {spawn, ChildProcess} from 'child_process'
 
 class ApiServer {
@@ -21,9 +22,11 @@ class ApiServer {
     this._spawn.on('close', function (code) {
       if (code !== 0) return console.error(new Error('API finished with non-zero exit code (' + code + ')'));
       console.log('API closed.');
-    })
+    });
   
-    this._spawn.on('error', console.error);
+    this._spawn.on('error', (err) => {
+      throw new Error('Unable to start API: ' + err.message);
+    });
   }
   
   static stop(): void {
@@ -50,9 +53,11 @@ class MongoDb {
     this._spawn.on('close', function (code) {
       if (code !== 0) return console.error(new Error('MongoDB finished with non-zero exit code (' + code + ')'));
       console.log('MongoDB closed.');
-    })
+    });
   
-    this._spawn.on('error', console.error);
+    this._spawn.on('error', (err) => {
+      throw new Error('Unable to start MongoDB: ' + err.message);
+    });
   }
   
   static stop(): void {
